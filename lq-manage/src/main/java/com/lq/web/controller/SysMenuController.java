be498a1a16212a837fd2e364cb53717f0f603acc
shadowLQ <415804410@qq.com>
2021-10-30 20:45:03 +0800

    加入短信服务、谷歌工具包、新增字典添加接口

---
+++ lq-manage/src/main/java/com/lq/web/controller/SysMenuController.java
@@ -0,0 +1,21 @@
+package com.lq.web.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ * 菜单权限表 前端控制器
+ * </p>
+ *
+ * @author LQ
+ * @since 2021-10-07
+ */
+@RestController
+@RequestMapping("/sysMenu")
+public class SysMenuController {
+
+}
+
---
+++ lq-system/src/main/java/com/lq/system/mapper/SysMenuMapper.java
@@ -0,0 +1,17 @@
+package com.lq.system.mapper;
+
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.lq.system.entity.SysMenu;
+
+/**
+ * <p>
+ * 菜单权限表 Mapper 接口
+ * </p>
+ *
+ * @author LQ
+ * @since 2021-10-07
+ */
+public interface SysMenuMapper extends BaseMapper<SysMenu> {
+
+}
